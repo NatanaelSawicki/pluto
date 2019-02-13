0966086b602ae1176cdf76462bd5ba6e8cce4cf0
NicolasCunha <nicolascunha17@gmail.com>
2019-02-13 20:16:31 -0200

    Created an logging class for arrays. Used in tests when developing sorting features.

---
+++ src/main/java/sorting/common/ArrayLogger.java
@@ -0,0 +1,48 @@
+package sorting.common;
+
+import logger.LoggingHandler;
+
+public class ArrayLogger {
+    
+    public static void log(Object[] array){
+        LoggingHandler.log("LOGGING ARRAY", false);
+        LoggingHandler.log("ARRAY LENGTH: "+array.length, false);
+        LoggingHandler.log("ARRAY CONTENT - BEGIN: ", false);
+        for(Object iterador : array){
+            LoggingHandler.log((String) iterador, false);
+        }
+        LoggingHandler.log("ARRAY CONTENT - END: ", false);
+    }
+    
+    public static void log(String[] array){
+        LoggingHandler.log("LOGGING ARRAY", false);
+        LoggingHandler.log("ARRAY LENGTH: "+array.length, false);
+        LoggingHandler.log("ARRAY CONTENT - BEGIN: ", false);
+        for(String iterador : array){
+            LoggingHandler.log(iterador, false);
+        }
+        LoggingHandler.log("ARRAY CONTENT - END: ", false);
+    }
+    
+    
+    public static void log(Long[] array){
+        LoggingHandler.log("LOGGING ARRAY", false);
+        LoggingHandler.log("ARRAY LENGTH: "+array.length, false);
+        LoggingHandler.log("ARRAY CONTENT - BEGIN: ", false);
+        for(Long iterador : array){
+            LoggingHandler.log(String.valueOf(iterador), false);
+        }
+        LoggingHandler.log("ARRAY CONTENT - END: ", false);
+    }
+    
+    public static void log(Double[] array){
+        LoggingHandler.log("LOGGING ARRAY", false);
+        LoggingHandler.log("ARRAY LENGTH: "+array.length, false);
+        LoggingHandler.log("ARRAY CONTENT - BEGIN: ", false);
+        for(Double iterador : array){
+            LoggingHandler.log(String.valueOf(iterador), false);
+        }
+        LoggingHandler.log("ARRAY CONTENT - END: ", false);
+    }
+    
+}
