5abda2b7fc2f7d7736b44d56665c3c2d0f3bf596
NicolasCunha <nicolascunha17@gmail.com>
2019-02-13 20:07:12 -0200

    Added a missing 'g' to the LoggingHandler class.

---
+++ src/main/java/logger/LoggingHandler.java
@@ -0,0 +1,38 @@
+package logger;
+
+import date.DateHandler;
+import java.util.ArrayList;
+
+public class LoggingHandler {
+    
+    private static ArrayList<String> logginCache = new ArrayList<>();
+
+    public static ArrayList<String> getLogginCache() {
+        return logginCache;
+    }
+
+    public static void setLogginCache(ArrayList<String> logginCache) {
+        LoggingHandler.logginCache = logginCache;
+    }
+
+    public static void log(String logMessage, boolean isError) {
+        String sLog;
+
+        if (logMessage.isEmpty()) {
+            sLog = "[LOG_ERROR] [" + DateHandler.getCurrentDateString() + "] Tried to log an empty message.";
+        } else {
+            if (isError) {
+                sLog = "[ERROR] " + logMessage;
+            } else {
+                sLog = "[ERROR] " + logMessage;
+            }
+        }
+
+        if (sLog != null) {
+            logginCache.add(sLog);
+            System.out.println(sLog);
+        }
+
+    }
+
+}
