facbcb42b635337570051536ff3ad2ca68ec1fff
NicolasCunha <nicolascunha17@gmail.com>
2019-02-16 16:32:22 -0200

    Adjusted SelectionSort to accept only classes that can be comparable.

--- src/main/java/sorting/algorithms/selectionsort/SelectionSort.java
+++ src/main/java/sorting/algorithms/selectionsort/SelectionSort.java
@@ -1,61 +1,31 @@
 package sorting.algorithms.selectionsort;
 
-public class SelectionSort {
-    
-    public static Long[] applySelectionSortAscend(Long[] parameterArray){
+public class SelectionSort<T extends Comparable<T>> {
+
+    public T[] applySelectionSortAscend(T[] parameterArray) {
         for (int i = 0; i < parameterArray.length; i++) {
             int lesserPosition = i;
             for (int j = i; j < parameterArray.length; j++) {
-                if(parameterArray[j] < parameterArray[lesserPosition]){
+                if (parameterArray[j].compareTo(parameterArray[lesserPosition]) < 0) {
                     lesserPosition = j;
                 }
             }
-            long temporarySwapVariable = parameterArray[i];
+            T temporarySwapVariable = parameterArray[i];
             parameterArray[i] = parameterArray[lesserPosition];
             parameterArray[lesserPosition] = temporarySwapVariable;
         }
         return parameterArray;
     }
-    
-    public static Long[] applySelectionSortDescend(Long[] parameterArray){
+
+    public T[] applySelectionSortDescend(T[] parameterArray) {
         for (int i = 0; i < parameterArray.length; i++) {
             int greaterPosition = i;
             for (int j = i; j < parameterArray.length; j++) {
-                if(parameterArray[j] > parameterArray[greaterPosition]){
+                if (parameterArray[j].compareTo(parameterArray[greaterPosition]) > 0) {
                     greaterPosition = j;
                 }
             }
-            long temporarySwapVariable = parameterArray[i];
-            parameterArray[i] = parameterArray[greaterPosition];
-            parameterArray[greaterPosition] = temporarySwapVariable;
-        }
-        return parameterArray;
-    }
-    
-    public static Double[] applySelectionSortAscend(Double[] parameterArray){
-        for (int i = 0; i < parameterArray.length; i++) {
-            int lesserPosition = i;
-            for (int j = i; j < parameterArray.length; j++) {
-                if(parameterArray[j] < parameterArray[lesserPosition]){
-                    lesserPosition = j;
-                }
-            }
-            Double temporarySwapVariable = parameterArray[i];
-            parameterArray[i] = parameterArray[lesserPosition];
-            parameterArray[lesserPosition] = temporarySwapVariable;
-        }
-        return parameterArray;
-    }
-    
-    public static Double[] applySelectionSortDescend(Double[] parameterArray){
-        for (int i = 0; i < parameterArray.length; i++) {
-            int greaterPosition = i;
-            for (int j = i; j < parameterArray.length; j++) {
-                if(parameterArray[j] > parameterArray[greaterPosition]){
-                    greaterPosition = j;
-                }
-            }
-            Double temporarySwapVariable = parameterArray[i];
+            T temporarySwapVariable = parameterArray[i];
             parameterArray[i] = parameterArray[greaterPosition];
             parameterArray[greaterPosition] = temporarySwapVariable;
         }
