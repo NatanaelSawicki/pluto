6f59672a6b4b9a67bb9027eb4e1fb2d96410914c
NicolasCunha <nicolascunha17@gmail.com>
2019-02-16 15:07:48 -0200

    Implemented Queue node class.

--- src/main/java/datastructures/common/QueueNode.java
+++ src/main/java/datastructures/common/QueueNode.java
@@ -6,13 +6,31 @@ import serializer.SerializeHandler;
 
 public class QueueNode<T> implements Serializable {
 
-    
-    private T data;
-    
-    
-    
-    public String serialize() throws PlutoIOException{
-        return SerializeHandler.serialize(this);
+    private T objectValue;
+    private QueueNode<T> nextNode;
+
+    public QueueNode(T objectData){
+        setObjectValue(objectData);
     }
     
+    public T getObjectValue() {
+        return objectValue;
+    }
+
+    public void setObjectValue(T objectValue) {
+        this.objectValue = objectValue;
+    }
+
+    public QueueNode<T> getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(QueueNode<T> nextNode) {
+        this.nextNode = nextNode;
+    }
+
+    public String serialize() throws PlutoIOException {
+        return SerializeHandler.serialize(this);
+    }
+
 }
