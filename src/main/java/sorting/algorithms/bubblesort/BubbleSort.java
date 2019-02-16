45f5ca8f29e4cfa5d7e3e871a5b72ceba872f846
NicolasCunha <nicolascunha17@gmail.com>
2019-02-16 16:32:07 -0200

    Adjusted BubbleSort to accept only classes that can be comparable.

--- src/main/java/sorting/algorithms/bubblesort/BubbleSort.java
+++ src/main/java/sorting/algorithms/bubblesort/BubbleSort.java
@@ -1,12 +1,15 @@
 package sorting.algorithms.bubblesort;
 
-public class BubbleSort {
+import sorting.common.ArrayGenerator;
+import sorting.common.ArrayLogger;
 
-    public static Long[] applyBubbleSortAscend(Long[] array) {
+public class BubbleSort<T extends Comparable<T>> {
+
+    public T[] applyBubbleSortAscend(T[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    long temp = array[j];
+                if (array[j].compareTo(array[j + 1]) > 0) {
+                    T temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
@@ -16,46 +19,17 @@ public class BubbleSort {
         return array;
     }
 
-    public static Long[] applyBubbleSortDescend(Long[] array) {
+    public T[] applyBubbleSortDescend(T[] array) {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] < array[j + 1]) {
-                    long temp = array[j];
+                if (array[j].compareTo(array[j + 1]) < 0) {
+                    T temp = array[j];
                     array[j] = array[j + 1];
                     array[j + 1] = temp;
                 }
             }
         }
-
         return array;
     }
-
-    public static Double[] applyBubbleSortAscend(Double[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] > array[j + 1]) {
-                    Double temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
-
-        return array;
-    }
-
-    public static Double[] applyBubbleSortDescend(Double[] array) {
-        for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length - 1; j++) {
-                if (array[j] < array[j + 1]) {
-                    Double temp = array[j];
-                    array[j] = array[j + 1];
-                    array[j + 1] = temp;
-                }
-            }
-        }
-
-        return array;
-    }
-
+    
 }
