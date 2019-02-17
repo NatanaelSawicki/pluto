52684b9033f5f7794431c8a154b224045a06de2b
NicolasCunha <nicolascunha17@gmail.com>
2019-02-17 15:14:09 -0300

    Implemented BinaryTree node class.

--- src/main/java/datastructures/common/BinaryTreeNode.java
+++ src/main/java/datastructures/common/BinaryTreeNode.java
@@ -1,5 +1,53 @@
 package datastructures.common;
 
-public class BinaryTreeNode {
-    // TODO implement BinaryTreeNode class
+public class BinaryTreeNode<T> {
+
+    private T nodeValue;
+    private BinaryTreeNode<T> leftNode;
+    private BinaryTreeNode<T> rightNode;
+
+    public BinaryTreeNode(T parameterValue) {
+        setNodeValue(parameterValue);
+    }
+
+    public T getNodeValue() {
+        return nodeValue;
+    }
+
+    public void setNodeValue(T nodeValue) {
+        this.nodeValue = nodeValue;
+    }
+
+    public BinaryTreeNode<T> getLeftNode() {
+        return leftNode;
+    }
+
+    public void setLeftNode(BinaryTreeNode<T> leftNode) {
+        this.leftNode = leftNode;
+    }
+
+    public BinaryTreeNode<T> getRightNode() {
+        return rightNode;
+    }
+
+    public void setRightNode(BinaryTreeNode<T> rightNode) {
+        this.rightNode = rightNode;
+    }
+
+    public boolean onlyHasRightNode() {
+        return getRightNode() != null && getLeftNode() == null;
+    }
+
+    public boolean onlyHasLeftNode() {
+        return getLeftNode() != null && getRightNode() == null;
+    }
+
+    public boolean hasNoChildNode() {
+        return getLeftNode() == null && getRightNode() == null;
+    }
+
+    public boolean hasBothChildNodes() {
+        return !hasNoChildNode();
+    }
+
 }
